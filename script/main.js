64065fb62c1b2701168f77d7025fa37471caecb8
Murilo Barbosa <murilobarbosadarosa2002@gmail.com>
2022-06-19 14:43:46 -0300

    Conclusão do Projeto
    Commit com as ultimas alterações do projeto.

--- script/main.js
+++ script/main.js
@@ -25,6 +25,42 @@ function checkInputs(){
         setSuccessFor(userName);
     }
 
+    if (userEmailValue === '') {
+
+        setErrorFor(userEmail, 'O Email é obrigatório...')
+    } else if(!checkEmail(userEmailValue)){
+
+        setErrorFor(userEmail, 'Por favor insira um Email valido...')
+    }
+    
+    else {
+
+        setSuccessFor(userEmail)
+    }
+
+    if(userPasswordValue === '') {
+
+        setErrorFor(userPassword, 'A senha é obrigatória...')
+    } else if(userPasswordValue.length < 7) {
+
+        setErrorFor(userPassword, 'A senha tem que ter pelo menos 7 caracteres...')
+    } else {
+        setSuccessFor(userPassword)
+    }
+
+    if (passwordConfirmValue === '') {
+
+        setErrorFor(passwordConfirm, 'A confirmação de senha é obrigatória...')
+    } else if(passwordConfirmValue !== userPasswordValue) {
+        setErrorFor(passwordConfirm, 'As senhas não são iguais...')
+    } else {
+
+        setSuccessFor(passwordConfirm)
+    }
+
+
+
+
 }
 
 function setSuccessFor(input){
@@ -43,3 +79,9 @@ function setErrorFor(input, message){
     formControl.className = 'form-control error'
 
 }
+
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
+  }
